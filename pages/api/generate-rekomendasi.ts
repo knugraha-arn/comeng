a88d76a6cbaf0337228f95779bec58eae591ac58
knugraha-arn <knugraha@arranetwork.com>
2026-06-01 15:43:31 +0700

    Update generate-rekomendasi.ts

--- pages/api/generate-rekomendasi.ts
+++ pages/api/generate-rekomendasi.ts
@@ -1,6 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { createClient } from '@supabase/supabase-js'
 
+export const config = {
+  maxDuration: 30,
+}
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_ROLE_KEY!
